08c5e51570413836df9ef47ba0ac2aeeab3d7b1c
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-11 21:29:13 +0800

    add appbar and update function

--- lib/screen/settings_page.dart
+++ lib/screen/settings_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_app/common/color.dart';
 import 'package:restaurant_app/provider/daily_alarm_provider.dart';
 import 'package:restaurant_app/screen/search_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -41,36 +43,63 @@ class _SettingPageState extends State<SettingPage> {
     return ChangeNotifierProvider<SchedulingProvider>(
       create: (context) => SchedulingProvider(),
       child: Scaffold(
-        appBar: AppBar(title: Text("setting")),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          title: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "Search Restaurant",
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[
+                  CustomColor().fieryRose,
+                  CustomColor().princetonOrange,
+                  CustomColor().tartOrange,
+                ],
+              ),
+            ),
+          ),
+          elevation: 0,
+        ),
         body: Column(
           children: [
             const SizedBox(
               height: 20.0,
             ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Material(
-              child: ListTile(
-                iconColor: Colors.black,
-                title: const Text('Push Notification'),
-                subtitle: const Text('Restaurant Recommendation'),
-                trailing: Consumer<SchedulingProvider>(
-                  builder: (context, scheduled, _) {
-                    return Switch(
-                      value: notification,
-                      onChanged: (bool value) {
-                        setState(
-                          () {
-                            scheduled.scheduledRestaurantRecoment(value);
-                            scheduled.scheduledStatus;
-                            onChanged(value);
-                          },
-                        );
-                      },
-                    );
-                  },
+            ListTile(
+              iconColor: Colors.black,
+              title: Text(
+                'Push Notification',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
                 ),
+              ),
+              trailing: Consumer<SchedulingProvider>(
+                builder: (context, scheduled, _) {
+                  return Switch(
+                    activeColor: CustomColor().fieryRose,
+                    value: notification,
+                    onChanged: (value) {
+                      setState(
+                        () {
+                          scheduled.scheduledRestaurantRecoment(value);
+                          scheduled.scheduledStatus;
+                          onChanged(value);
+                        },
+                      );
+                    },
+                  );
+                },
               ),
             )
           ],
