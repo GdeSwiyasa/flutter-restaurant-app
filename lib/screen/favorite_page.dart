aa520147e5b73853218f9c7f51358d4811943b3d
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-11 21:28:16 +0800

    add appbar

--- lib/screen/favorite_page.dart
+++ lib/screen/favorite_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_app/common/color.dart';
 import 'package:restaurant_app/provider/favorite_provider.dart';
 import 'package:restaurant_app/widget/custom_fav_card.dart';
 
@@ -11,34 +13,67 @@ class FavoritePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("favorite"),
+        toolbarHeight: 100,
+        title: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            "Favorite Restaurant",
+            style: GoogleFonts.poppins(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+                CustomColor().fieryRose,
+                CustomColor().princetonOrange,
+                CustomColor().tartOrange,
+              ],
+            ),
+          ),
+        ),
+        elevation: 0,
       ),
-      body: Column(
-        children: [
-          Expanded(child: Consumer<FavoriteProvider>(
-            builder: ((context, value, child) {
-              if (value.data == ResultState.loading) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (value.data == ResultState.hasData) {
-                return ListView.separated(
-                    itemBuilder: (context, index) {
-                      var restaurant = value.listFavorite[index];
-                      return CustomFavCard(restaurant: restaurant);
-                    },
-                    separatorBuilder: (context, index) => const SizedBox(
-                          height: 24.0,
-                        ),
-                    itemCount: value.listFavorite.length);
-              } else {
-                return Center(
-                  child: Text("Ups"),
-                );
-              }
-            }),
-          ))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(child: Consumer<FavoriteProvider>(
+              builder: ((context, value, child) {
+                if (value.data == ResultState.loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (value.data == ResultState.hasData) {
+                  return ListView.separated(
+                      itemBuilder: (context, index) {
+                        var restaurant = value.listFavorite[index];
+                        return CustomFavCard(restaurant: restaurant);
+                      },
+                      separatorBuilder: (context, index) => const SizedBox(
+                            height: 24.0,
+                          ),
+                      itemCount: value.listFavorite.length);
+                } else {
+                  return Center(
+                    child: Text(
+                      "No Data Here",
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  );
+                }
+              }),
+            ))
+          ],
+        ),
       ),
     );
   }
