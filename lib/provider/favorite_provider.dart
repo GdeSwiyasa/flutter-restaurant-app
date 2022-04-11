f6f87f2db348d4ca26ff2d01e564291265a583d3
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-11 11:06:46 +0800

    update favorite provider

---
+++ lib/provider/favorite_provider.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class FavoriteProvider extends ChangeNotifier {
+  final DatabaseService databaseService;
+
+  FavoriteProvider({required this.databaseService}) {
+    getFavorite();
+  }
+
+  ResultState _data = ResultState.hasData;
+  String _message = '';
+  List<RestaurantFavorit> _listFavorite = [];
+
+  ResultState get data => _data;
+  String get message => _message;
+  List<RestaurantFavorit> get listFavorite => _listFavorite;
+
+  void getFavorite() async {
+    _listFavorite = await databaseService.getFavorite();
+    if (_listFavorite.isNotEmpty) {
+      _data = ResultState.hasData;
+    } else {
+      _data = ResultState.noData;
+      _message = 'Empty Data';
+    }
+    notifyListeners();
+  }
+
+  void addFavorite(RestaurantDetail restautant) async {
+    try {
+      await databaseService.insertFavorit(restautant);
+      getFavorite();
+    } catch (e) {
+      _data = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+
+  Future<bool> isFavorite(RestaurantDetail restaurant) async {
+    final favorite = await databaseService.getFavoriteById(restaurant.id);
+    return favorite.isNotEmpty;
+  }
+
+  void removeFav(RestaurantDetail restaurant) async {
+    try {
+      await databaseService.removeFavorite(restaurant.id);
+      getFavorite();
+    } catch (e) {
+      _data = ResultState.error;
+      _message = 'Error: $e';
+      notifyListeners();
+    }
+  }
+}
+
+enum ResultState { loading, noData, hasData, error }
