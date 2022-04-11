029fb8e1489283011efabddbf0791ef040e09884
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-11 18:17:04 +0800

    remove splashscreen route

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
+import 'dart:io';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/data/api/database_service.dart';
+import 'package:restaurant_app/data/utils/background_service.dart';
 import 'package:restaurant_app/data/utils/notification_helper.dart';
 import 'package:restaurant_app/provider/favorite_provider.dart';
 import 'package:restaurant_app/screen/detailpage.dart';
 import 'package:restaurant_app/screen/favorite_page.dart';
 import 'package:restaurant_app/screen/homepage.dart';
 import 'package:restaurant_app/screen/search_page.dart';
+import 'package:restaurant_app/screen/settings_page.dart';
 
 import 'package:restaurant_app/screen/splashscreen.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final NotificationHelper _notificationHelper = NotificationHelper();
+  final BackgroundService _service = BackgroundService();
+  _service.initializeIsolate();
+  if (Platform.isAndroid) {
+    await AndroidAlarmManager.initialize();
+  }
   await _notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
-
   runApp(MyApp());
 }
 
@@ -30,9 +40,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
-        initialRoute: SplashScreen.routeName,
+        initialRoute: HomePage.routeName,
         routes: {
-          SplashScreen.routeName: (context) => SplashScreen(),
+          // SplashScreen.routeName: (context) => SplashScreen(),
           HomePage.routeName: (context) => HomePage(),
           DetailPage.routeName: (context) => DetailPage(
                 restaurant_id:
@@ -40,6 +50,7 @@ class MyApp extends StatelessWidget {
               ),
           SearchPage.routeName: (context) => SearchPage(),
           FavoritePage.routeName: (context) => FavoritePage(),
+          SettingPage.routeName: (context) => SettingPage(),
         },
       ),
     );
