f0bc8ca1ff7eb0e41d808f955607043d406a992c
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-11 11:38:25 +0800

    create widget custom card

---
+++ lib/widget/custom_fav_card.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restaurant_app/common/color.dart';
+import 'package:restaurant_app/data/model/favorite_model.dart';
+import 'package:restaurant_app/screen/detailpage.dart';
+
+import '../data/model/restaurant_model.dart';
+
+class CustomCard extends StatelessWidget {
+  final RestaurantFavorit restaurant;
+
+  const CustomCard({Key? key, required this.restaurant}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(8),
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          DetailPage.routeName,
+          arguments: restaurant.id,
+        );
+      },
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(
+                child: Container(
+                  padding: const EdgeInsets.only(right: 30),
+                  child: Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Stack(
+                        children: [
+                          Hero(
+                            tag: restaurant.pictureId,
+                            child: Image.network(
+                              "https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}",
+                              width: 130,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 1),
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: CustomColor()
+                                  .princetonOrange
+                                  .withOpacity(0.9),
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(12),
+                                bottomRight: Radius.circular(12),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                const Icon(
+                                  Icons.star,
+                                  size: 15,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  restaurant.rating.toString(),
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      restaurant.name,
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    Text(
+                      restaurant.description,
+                      style: GoogleFonts.poppins(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Start from 35rb",
+                          style: GoogleFonts.poppins(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "3.2Km Distance",
+                          style: GoogleFonts.poppins(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8, vertical: 1),
+                          decoration: BoxDecoration(
+                              color: CustomColor().blueLight.withOpacity(0.5),
+                              borderRadius: BorderRadius.circular(100)),
+                          child: Text(
+                            "30 % Discount",
+                            style: GoogleFonts.poppins(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+        ],
+      ),
+    );
+  }
+}
